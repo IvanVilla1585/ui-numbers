08bdd15304e70bcfd068054b46184f322f8489ba
Ivan Villa <trezeguet55@gmail.com>
2018-03-25 20:19:24 -0500

    <src index.js>
    render basic component

---
+++ src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.scss'
+
+const App = () => (
+  <h1>Sapos</h1>
+);
+
+ReactDOM.render(<App/>, document.getElementById('app'))
